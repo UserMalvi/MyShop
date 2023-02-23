ac21e0a181fcd6f4b415c8f8933a1f15d708b6f5
UserMalvi <mrathod@aakashinformatics.com>
2023-02-23 14:39:47 +0530

    commiting mocks

---
+++ Myshop.WebUi.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Myshop.WebUi.Controllers;
+using Myshop.WebUi.Tests.Mocks;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Services;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Myshop.WebUi.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            //basketService.AddToBasket(httpContext, "1"); 
+
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+
+
+        }
+            [TestMethod]
+            public void CanGetSummaryViewModel()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.insert(new Product() { Id = "1", Price = 10.00m });
+            products.insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+
+            //IBasketService
+
+            //var IBasketService basketService = null;
+            //Controller = new BasketController(basketService);
+            //var httpContext = new MockHttpContext();
+            //httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerce") { Value = basket.Id });
+            //Controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            //var result = Controller.BasketSummary() as PartialViewResult;
+            //Assert.AreEqual(3, basketSummary.BasketCount)
+        }
+    }
+}
--- Myshop.WebUi.Tests/Controllers/HomeControllerTest.cs
+++ Myshop.WebUi.Tests/Controllers/HomeControllerTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Myshop.WebUi;
 using Myshop.WebUi.Controllers;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +16,26 @@ namespace Myshop.WebUi.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPagesDoesreturnProducts()
         {
             //// Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            productContext.insert(new Product());
+
+            var result = controller.Index() as ViewResult;
+            var ViewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, ViewModel.Products.Count());
 
             //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+           // ViewResult result = controller.Index() as ViewResult;
 
             //// Assert
             //Assert.IsNotNull(result);
+            ///Assert.IsTrue(1 == 1);
         }
 
       
